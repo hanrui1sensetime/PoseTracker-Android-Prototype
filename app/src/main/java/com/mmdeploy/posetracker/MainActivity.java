a78687ae6f3bb6a125da1cd3cfa5d1885b02bb72
hanrui1sensetime <hanrui1@sensetime.com>
2023-03-09 17:09:24 +0800

    refactor draw

--- app/src/main/java/com/mmdeploy/posetracker/MainActivity.java
+++ app/src/main/java/com/mmdeploy/posetracker/MainActivity.java
@@ -1,7 +1,5 @@
 package com.mmdeploy.posetracker;
 
-import static java.lang.Math.round;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,9 +20,6 @@ import android.widget.Toast;
 import android.database.Cursor;
 import android.provider.MediaStore;
 
-import org.opencv.core.Point;
-import org.opencv.core.Scalar;
-import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.videoio.*;
 
@@ -149,69 +144,7 @@ public class MainActivity extends AppCompatActivity {
                     Imgproc.cvtColor(frame, cvMat, Imgproc.COLOR_RGB2BGR);
                     Mat mat = Utils.cvMatToMat(cvMat);
                     mmdeploy.PoseTracker.Result[] results = poseTracker.apply(stateHandle, mat, -1);
-                    int skeleton[][] = {{15, 13}, {13, 11}, {16, 14}, {14, 12}, {11, 12}, {5, 11}, {6, 12},
-                            {5, 6}, {5, 7}, {6, 8}, {7, 9}, {8, 10}, {1, 2}, {0, 1},
-                            {0, 2}, {1, 3}, {2, 4}, {3, 5}, {4, 6}};
-                    Scalar palette[] = {new Scalar(255, 128, 0), new Scalar(255, 153, 51), new Scalar(255, 178, 102),
-                            new Scalar(230, 230, 0), new Scalar(255, 153, 255), new Scalar(153, 204, 255),
-                            new Scalar(255, 102, 255), new Scalar(255, 51, 255), new Scalar(102, 178, 255),
-                            new Scalar(51, 153, 255), new Scalar(255, 153, 153), new Scalar(255, 102, 102),
-                            new Scalar(255, 51, 51), new Scalar(153, 255, 153), new Scalar(102, 255, 102),
-                            new Scalar(51, 255, 51), new Scalar(0, 255, 0), new Scalar(0, 0, 255),
-                            new Scalar(255, 0, 0), new Scalar(255, 255, 255)};
-                    int linkColor[] = {
-                            0, 0, 0, 0, 7, 7, 7, 9, 9, 9, 9, 9, 16, 16, 16, 16, 16, 16, 16
-                    };
-                    int pointColor[] = {16, 16, 16, 16, 16, 9, 9, 9, 9, 9, 9, 0, 0, 0, 0, 0, 0};
-
-                    float scale = 1280 / (float)Math.max(frame.cols(), frame.rows());
-                    if (scale != 1) {
-                        Imgproc.resize(frame, frame, new Size(), scale, scale);
-                    }
-                    else
-                    {
-                        frame = frame.clone();
-                    }
-                    for (int i = 0; i < results.length; i++)
-                    {
-                        mmdeploy.PoseTracker.Result pt = results[i];
-                        for (int j = 0; j < pt.keypoints.length; j++)
-                        {
-                            PointF p = pt.keypoints[j];
-                            p.x *= scale;
-                            p.y *= scale;
-                            pt.keypoints[j] = p;
-                        }
-                        float scoreThr = 0.5f;
-                        int used[] = new int[pt.keypoints.length * 2];
-                        for (int j = 0; j < skeleton.length; j++)
-                        {
-                            int u = skeleton[j][0];
-                            int v = skeleton[j][1];
-                            if (pt.scores[u] > scoreThr && pt.scores[v] > scoreThr)
-                            {
-                                used[u] = used[v] = 1;
-                                Point pointU = new Point(pt.keypoints[u].x, pt.keypoints[u].y);
-                                Point pointV = new Point(pt.keypoints[v].x, pt.keypoints[v].y);
-                                Imgproc.line(frame, pointU, pointV, palette[linkColor[j]], 4);
-                            }
-                        }
-                        for (int j = 0; j < pt.keypoints.length; j++)
-                        {
-                            if (used[j] == 1)
-                            {
-                                Point p = new Point(pt.keypoints[j].x, pt.keypoints[j].y);
-                                Imgproc.circle(frame, p, 1, palette[pointColor[j]], 2);
-                            }
-                        }
-                        float bbox[] = {pt.bbox.left, pt.bbox.top, pt.bbox.right, pt.bbox.bottom};
-                        for (int j = 0; j < 4; j++)
-                        {
-                            bbox[j] *= scale;
-                        }
-                        Imgproc.rectangle(frame, new Point(bbox[0], bbox[1]),
-                                new Point(bbox[2], bbox[3]), new Scalar(0, 255, 0));
-                    }
+                    Draw.drawPoseTrackerResult(frame, results);
                     Bitmap bitmap = null;
                     bitmap = Bitmap.createBitmap(frame.width(), frame.height(), Bitmap.Config.ARGB_8888);
                     org.opencv.android.Utils.matToBitmap(frame, bitmap);
